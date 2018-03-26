9202130ad1f2685b19516f155db9061e6f0efdcb
gbmahili <georgesmahili@gmail.com>
2018-03-25 21:41:20 -0400

    Added NYTimes api and route

--- controllers/api/saved.js
+++ controllers/api/saved.js
@@ -1,11 +1,26 @@
 // require express
 const express = require("express");
+const request = require("request");
 // Export the saved query
 module.exports = app => {
     const NYT_API_KEY = "ddab414e9a924d75bc1cd66ae4933b41";
-    // Get saved articles
+
+
     app.get("/api/saved", function(req, res){
-        
-        console.log(req);
+
+        request.get({
+            url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
+            qs: {
+                'api-key': NYT_API_KEY,
+                'q': "Tesla",
+                'begin_date': "20150101",
+                'end_date': "20180101",
+                'page': 1,
+                'fl': 'web_url, headline'
+            },
+        }, function (err, response, body) {
+            body = JSON.parse(body);
+            res.json(body);
+        });
     })
 }
